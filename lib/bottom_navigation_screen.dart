4a21922b1ceed80b18e3ab5e4f74b2967666bf1b
faiza2710 <faizaafzal487@gmail.com>
2025-06-30 09:42:13 +0500

    bottom bar

--- lib/bottom_navigation_screen.dart
+++ lib/bottom_navigation_screen.dart
@@ -4,6 +4,7 @@ import 'package:fyp/education/NewHomePage.dart';
 import 'package:fyp/sample.dart';
 import 'package:image_picker/image_picker.dart';
 import 'User_profile.dart';
+import 'community/community_screen.dart';
 import 'todo_screens/home.dart';
 import 'emergency_screens/EmergencyPage.dart';
 
@@ -18,7 +19,7 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
   static List<Widget> _pages = <Widget>[
     HomePage(),
     TodoListPage(),
-    Sample(),
+    CommunityScreen(),
     HomeServicesPage(),
     // EmergencyPage(),
     UserProfilePage(),
@@ -26,7 +27,10 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
 
   void _onItemTapped(int index) {
     if (index == 2) {
-      _showModalBottomSheet(); // Show the modal bottom sheet when the "add" button is tapped
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => CommunityScreen()),
+      );
     } else {
       setState(() {
         _selectedIndex = index;
@@ -35,73 +39,74 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
     }
   }
 
-  void _showModalBottomSheet() {
-    showModalBottomSheet(
-      backgroundColor: Colors.pink,
-      context: context,
-      builder: (BuildContext context) {
-        return SizedBox(
-          height: 150,
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Upload photo',
-                    style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                SizedBox(height: 15),
-                InkWell(
-                  onTap: () {
-                    _pickImage(ImageSource.gallery); // Open the gallery
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.photo_album_outlined,
-                        color: Colors.white,
-                      ),
-                      SizedBox(width: 20),
-                      Text('View photo library', style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 15),
-                InkWell(
-                  onTap: () {
-                    _pickImage(ImageSource.camera); // Open the camera
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.camera_alt_outlined,
-                        color: Colors.white,
-                      ),
-                      SizedBox(width: 20),
-                      Text('Take a photo', style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  final ImagePicker _picker = ImagePicker();
-  Future<void> _pickImage(ImageSource source) async {
-    final XFile? pickedFile = await _picker.pickImage(source: source);
-    if (pickedFile != null) {
-      // You can process the picked image (e.g., display it or upload it)
-      print('Picked image: ${pickedFile.path}');
-    }
-  }
+  //
+  // void _showModalBottomSheet() {
+  //   showModalBottomSheet(
+  //     backgroundColor: Colors.pink,
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return SizedBox(
+  //         height: 150,
+  //         child: Container(
+  //           padding: EdgeInsets.all(20),
+  //           child: Column(
+  //             children: [
+  //               SizedBox(height: 10),
+  //               Align(
+  //                 alignment: Alignment.centerLeft,
+  //                 child: Text(
+  //                   'Upload photo',
+  //                   style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w500),
+  //                 ),
+  //               ),
+  //               SizedBox(height: 15),
+  //               InkWell(
+  //                 onTap: () {
+  //                   _pickImage(ImageSource.gallery); // Open the gallery
+  //                 },
+  //                 child: Row(
+  //                   children: [
+  //                     Icon(
+  //                       Icons.photo_album_outlined,
+  //                       color: Colors.white,
+  //                     ),
+  //                     SizedBox(width: 20),
+  //                     Text('View photo library', style: TextStyle(color: Colors.white)),
+  //                   ],
+  //                 ),
+  //               ),
+  //               SizedBox(height: 15),
+  //               InkWell(
+  //                 onTap: () {
+  //                   _pickImage(ImageSource.camera); // Open the camera
+  //                 },
+  //                 child: Row(
+  //                   children: [
+  //                     Icon(
+  //                       Icons.camera_alt_outlined,
+  //                       color: Colors.white,
+  //                     ),
+  //                     SizedBox(width: 20),
+  //                     Text('Take a photo', style: TextStyle(color: Colors.white)),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
+  //
+  // final ImagePicker _picker = ImagePicker();
+  // Future<void> _pickImage(ImageSource source) async {
+  //   final XFile? pickedFile = await _picker.pickImage(source: source);
+  //   if (pickedFile != null) {
+  //     // You can process the picked image (e.g., display it or upload it)
+  //     print('Picked image: ${pickedFile.path}');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -112,59 +117,55 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
         child: _pages.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: Container(
-        height: 80.0,
+        height: MediaQuery.of(context).size.height * 0.15, // Responsive height
         color: Color.fromRGBO(36, 46, 73, 1),
         child: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           items: <BottomNavigationBarItem>[
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
               icon: Icon(Icons.home_sharp),
               label: '',
             ),
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
               icon: Icon(Icons.checklist),
               label: '',
             ),
             BottomNavigationBarItem(
               icon: Container(
-                padding: EdgeInsets.all(3), // Add padding
+                padding: EdgeInsets.all(3),
                 decoration: BoxDecoration(
-                  color: Colors.white, // White background
-                  //shape: BoxShape.circle, // Rounded borders
+                  color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Icon(
                   Icons.add,
-                  color: Colors.pink, // Icon color
-                  size: 25, // Icon size
+                  color: Colors.pink,
+                  size: 25,
                 ),
               ),
               label: '',
             ),
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
               icon: Icon(Icons.home_repair_service_sharp),
               label: '',
             ),
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
               icon: Icon(Icons.account_circle_outlined),
               label: '',
             ),
           ],
           currentIndex: _selectedIndex,
-          selectedItemColor: Colors.white, // Selected item color
-          unselectedItemColor: Colors.white70, // Unselected item color
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white70,
           onTap: _onItemTapped,
           backgroundColor: Colors.pink,
-          selectedIconTheme: IconThemeData(
-            size: 30,
-          ),
-          unselectedIconTheme: IconThemeData(
-            size: 25,
-          ),
-          showSelectedLabels: false, // Hide selected labels
-          showUnselectedLabels: false, // Hide unselected labels
+          selectedIconTheme: IconThemeData(size: 30),
+          unselectedIconTheme: IconThemeData(size: 25),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
         ),
       ),
+
     );
   }
 }
